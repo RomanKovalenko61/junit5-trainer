4e443bc7d76c379a99577c3f58611dea275d5d8c
RomanVM <astek14@mail.ru>
2026-09-08 14:50:11 +0300

    mapper tests

---
+++ src/test/java/com/dmdev/mapper/CreateUserMapperTest.java
@@ -0,0 +1,40 @@
+package com.dmdev.mapper;
+
+import com.dmdev.dto.CreateUserDto;
+import com.dmdev.entity.Gender;
+import com.dmdev.entity.Role;
+import com.dmdev.entity.User;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CreateUserMapperTest {
+
+    private final CreateUserMapper mapper = CreateUserMapper.getInstance();
+
+    @Test
+    void map() {
+        CreateUserDto dto = CreateUserDto.builder()
+                .name("Ivan")
+                .email("test@gmail.com")
+                .password("123")
+                .birthday("2000-01-01")
+                .role(Role.USER.name())
+                .gender(Gender.MALE.name())
+                .build();
+
+        User actual = mapper.map(dto);
+
+        User expected = User.builder()
+                .name("Ivan")
+                .email("test@gmail.com")
+                .password("123")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .role(Role.USER)
+                .gender(Gender.MALE)
+                .build();
+        assertThat(actual).isEqualTo(expected);
+    }
+}

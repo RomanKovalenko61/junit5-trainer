93c29f2cca6da3eb864c730f1158a66296432c9c
RomanVM <astek14@mail.ru>
2026-09-08 16:47:52 +0300

    UserService test method create

--- src/test/java/com/dmdev/service/UserServiceTest.java
+++ src/test/java/com/dmdev/service/UserServiceTest.java
@@ -1,13 +1,17 @@
 package com.dmdev.service;
 
 import com.dmdev.dao.UserDao;
+import com.dmdev.dto.CreateUserDto;
 import com.dmdev.dto.UserDto;
 import com.dmdev.entity.Gender;
 import com.dmdev.entity.Role;
 import com.dmdev.entity.User;
+import com.dmdev.exception.ValidationException;
 import com.dmdev.mapper.CreateUserMapper;
 import com.dmdev.mapper.UserMapper;
 import com.dmdev.validator.CreateUserValidator;
+import com.dmdev.validator.Error;
+import com.dmdev.validator.ValidationResult;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -18,6 +22,7 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -57,6 +62,43 @@ class UserServiceTest {
 
         assertThat(actual).isEmpty();
         verifyNoInteractions(userMapper);
+    }
+
+    @Test
+    void create() {
+        CreateUserDto createUserDto = getCreateUserDto();
+        User user = getUser();
+        UserDto userDto = getUserDto();
+        doReturn(new ValidationResult()).when(createUserValidator).validate(createUserDto);
+        doReturn(user).when(createUserMapper).map(createUserDto);
+        doReturn(userDto).when(userMapper).map(user);
+
+        UserDto actual = userService.create(createUserDto);
+
+        assertThat(actual).isEqualTo(userDto);
+        verify(userDao).save(user);
+    }
+
+    @Test
+    void shouldThrowExceptionIfDtoInvalid() {
+        CreateUserDto createUserDto = getCreateUserDto();
+        ValidationResult validationResult = new ValidationResult();
+        validationResult.add(Error.of("invalid...", "message"));
+        doReturn(validationResult).when(createUserValidator).validate(createUserDto);
+
+        assertThrows(ValidationException.class, () -> userService.create(createUserDto));
+        verifyNoInteractions(userDao, createUserMapper, userMapper);
+    }
+
+    private static CreateUserDto getCreateUserDto() {
+        return CreateUserDto.builder()
+                .name("Ivan")
+                .email("test@gmail.com")
+                .password("123")
+                .birthday("2000-01-01")
+                .role(Role.USER.name())
+                .gender(Gender.MALE.name())
+                .build();
     }
 
     private static UserDto getUserDto() {
